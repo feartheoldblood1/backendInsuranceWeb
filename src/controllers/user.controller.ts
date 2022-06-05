f07da3a74f3a991c2b6872b745c7dea934473048
feartheoldblood1 <p_lobachev@inbox.ru>
2022-06-05 14:08:35 +0300

    update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import {
   Count,
   CountSchema,
+  DateType,
   Filter, FilterExcludingWhere, repository, Where
 } from '@loopback/repository';
 import {
@@ -18,25 +19,39 @@ export class UserController {
     public userRepository: UserRepository,
   ) { }
 
-  @post('/users')
+  @post('/users/{mail}/{phone_number}/{password}/{surname}/{name}/{otchestvo}/{datebirth}/{foreigner}/{document}/{document_number}/{document_series}')
   @response(200, {
     description: 'User model instance',
     content: {'application/json': {schema: getModelSchemaRef(User)}},
   })
   async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(User, {
-            title: 'NewUser',
-            exclude: ['id'],
-          }),
-        },
-      },
-    })
-    user: Omit<User, 'id'>,
-  ): Promise<User> {
-    return this.userRepository.create(user);
+    @param.path.string('mail') p_mail: string,
+    @param.path.string('phone_number') p_phone_number: string,
+    @param.path.password('password') p_password: string,
+    @param.path.string('surname') p_surname: string,
+    @param.path.string('name') p_name: string,
+    @param.path.string('otchestvo') p_otchestvo: string,
+    @param.path.date('datebirth') p_datebirth: DateType,
+    @param.path.string('foreigner') p_foreigner: string,
+    @param.path.string('document') p_document: string,
+    @param.path.string('document_number') p_document_number: string,
+    @param.path.string('document_series') p_document_series: string,
+  ): Promise<Omit<User, 'id'>> {
+    let newUser = new User();
+
+    newUser.mail = p_mail;
+    newUser.phone_number = p_phone_number;
+    let hashPassword: string = SHA256(p_password).toString();
+    newUser.password = hashPassword;
+    newUser.surname = p_surname;
+    newUser.name = p_name;
+    newUser.otchestvo = p_otchestvo;
+    newUser.datebirth = p_datebirth.toString();
+    newUser.foreigner = p_foreigner;
+    newUser.document = p_document;
+    newUser.document_number = p_document_number;
+    newUser.document_series = p_document_series;
+    return this.userRepository.create(newUser);
   }
   @get('/users/{email}/{password}')
   @response(200, {
@@ -52,7 +67,7 @@ export class UserController {
   })
   async findByNamePass(
     @param.path.string('email') email: string,
-    @param.path.string('password') password: string,
+    @param.path.password('password') password: string,
   ): Promise<any> {
     const filter: Filter = {where: {mail: email}, fields: {password: true}}
     let userData: User[] = await this.userRepository.find(filter);
@@ -63,11 +78,6 @@ export class UserController {
     else {
       return false;
     }
-
-
-
-
-
   }
   @get('/users/count')
   @response(200, {
