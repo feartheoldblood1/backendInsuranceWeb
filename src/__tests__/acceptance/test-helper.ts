2ff0763fca7598850b7fc971e4f0719b3683c3e7
feartheoldblood1 <p_lobachev@inbox.ru>
2022-06-05 09:49:25 +0300

    first commit

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {BackendinsurancewebApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new BackendinsurancewebApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: BackendinsurancewebApplication;
+  client: Client;
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,91 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  mail: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone_number: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  surname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  otchestvo?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  datebirth: string;
+  @property({
+    type: 'string',
+    required: true,
+  })
+  foreigner: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  document: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  document_number: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  document_series: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
