f74a176329718b75498abeba56d3143a63a3cc17
feartheoldblood1 <p_lobachev@inbox.ru>
2022-06-19 22:08:56 +0300

    update

--- src/controllers/kasko.controller.ts
+++ src/controllers/kasko.controller.ts
@@ -21,7 +21,7 @@ export class KaskoController {
 
 
 
-  @post('/kasko/{registrAddress}/{markaCar}/{markaModelClass}/{dateCar}/{engineCapacity}/{bank}/{carCost}/{carPlace}/{userData}/{name}/{phone}')
+  @post('/kasko/{registrAddress}/{markaCar}/{markaModelClass}/{dateCar}/{engineCapacity}/{bank}/{carCost}/{carPlace}/{name}/{phone}')
   @response(200, {
     description: 'Ipoteka model instance',
     content: {'application/json': {schema: getModelSchemaRef(Ipoteka)}},
@@ -35,7 +35,7 @@ export class KaskoController {
     @param.path.string('bank') bank: string,
     @param.path.string('carCost') carCost: string,
     @param.path.string('carPlace') carPlace: string,
-    @param.path.string('userData') userData: string,
+
     @param.path.string('name') name: string,
     @param.path.string('phone') phone: string,
 
@@ -46,19 +46,20 @@ export class KaskoController {
       to: "p_lobachev@inbox.ru",
       subject: 'Оформление страхового полиса КАСКО',
       text: `Пришла заявка на покупку полиса КАСКО
-    Данные страхователя лица:
+      Данные страхователя лица:
     Имя: ${name}
     Номер телефона: ${phone}
     Адрес собственника: ${registrAddress}
-    Данные о транспортном средстве:
+
+      Данные о транспортном средстве:
     Марка автомобиля: ${markaCar}
     Модель и класс автомобиля: ${markaModelClass}
     Год выпуска: ${dateCar}
     Объем двигателя: ${engineCapacity}
-    Цена автомобилья: ${carCost}
+    Цена автомобиля: ${carCost}
     Банк кредитор: ${bank}
     Место использования автомобиля: ${carPlace}
-    Количество водителей ${userData}
+
 
     `
     }
